e9b2c5bc46f7497e4ce98f5969c08a32080db18a
grace luswanga <luswanga2@gmail.com>
2025-03-25 16:34:37 +0100

    fix: build errors

--- start/routes.ts
+++ start/routes.ts
@@ -7,13 +7,13 @@
 |
 */
 
-import AuthController from '#controllers/auth_controller'
-import HouseholdsController from '#controllers/households_controller'
+
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+import AuthController from '../app/controllers/auth_controller.js'
 
-router.post('/register', [AuthController, 'register']).as('auth.register')
-router.post('/login', [AuthController, 'login']).as('auth.login')
+router.post('auth/register', [AuthController, 'register']).as('auth.register')
+router.post('auth/login', [AuthController, 'login']).as('auth.login')
 router.delete('/logout', [AuthController, 'logout']).as('auth.logout').use(middleware.auth())
 
 
